36a52ef8a92b1d21a1b596416a496b914d890b59
wumengye <wumengye@cartx.ai>
2022-09-06 18:22:57 +0800

    增加重试util

---
+++ util/retry.go
@@ -0,0 +1,45 @@
+package util
+
+import (
+	"context"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/text/gstr"
+	"time"
+)
+
+const (
+	NoProvider      = "No provider available"
+	ProviderOffline = "transport: Error while dialing"
+	ErrorMessage    = "Rpc provider offline. Retrying %d times ..."
+)
+
+type ServiceRetry[Req any, Res any] struct {
+}
+
+func (r *ServiceRetry[Req, Res]) CallDubboGoFunc(ctx context.Context, rpcReq Req, rpcFunc func(context.Context, Req) (Res, error), retryLimit int, retryIntervalMillis int64) (Res, error) {
+	var (
+		rpcRes Res
+		err    error
+		retry  = 0
+	)
+	rpcRes, err = rpcFunc(ctx, rpcReq)
+	for err != nil && retry < retryLimit {
+		providerError1 := gstr.Pos(err.Error(), NoProvider) >= 0
+		providerError2 := gstr.Pos(err.Error(), ProviderOffline) >= 0
+		if providerError1 || providerError2 {
+			retry++
+			g.Log().Errorf(ctx, ErrorMessage, retry)
+			// 无限重试
+			time.Sleep(time.Duration(retryIntervalMillis) * time.Millisecond)
+			rpcRes, err = rpcFunc(ctx, rpcReq)
+		} else {
+			g.Log().Error(ctx, err)
+			retry++
+			g.Log().Errorf(ctx, ErrorMessage, retry)
+			// 无限重试
+			time.Sleep(time.Duration(retryIntervalMillis) * time.Millisecond)
+			rpcRes, err = rpcFunc(ctx, rpcReq)
+		}
+	}
+	return rpcRes, err
+}
